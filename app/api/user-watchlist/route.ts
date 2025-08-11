f4ea60f8da743babc003fa0ee3750f84c3428bea
Bswift305 <darrel.copper29@gmail.com>
2025-08-10 20:51:41 -0400

    Update route.ts

--- app/api/user-watchlist/route.ts
+++ app/api/user-watchlist/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getLimitFromSearchParams } from "../../../backend/utils/http";
-import { listGameScripts } from "../../../backend/services/gameScripts";
+import { getLimitFromSearchParams } from "@backend/utils/http";
+import { listUserWatchlist } from "@backend/services/userWatchlist";
 
 export async function GET(req: NextRequest) {
   try {
