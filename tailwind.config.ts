955fb5767d9dceacbde9394cfa0a1f869ae156ba
Bswift305 <darrel.copper29@gmail.com>
2025-08-14 17:49:06 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,6 @@
-# remove the broken TS file
-git rm tailwind.config.ts
-
-# write a JS config instead
-cat > tailwind.config.js <<'JS'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 };
-JS
